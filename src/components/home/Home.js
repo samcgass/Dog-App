98c64b5a2699c9cf8623d6318014a9e5866c6987
Sam Gass <samcgass@gmail.com>
2022-04-13 09:51:32 -0500

    Add content and video

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Lj8-1dvazAU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+        </div>
     )
 }

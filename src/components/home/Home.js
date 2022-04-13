08d55d5a4f9c095e3ada91698cb41ee9b0ea9cc6
Sam Gass <samcgass@gmail.com>
2022-04-13 10:30:11 -0500

    fix attribute names

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,7 +13,7 @@ export const Home = () => {
                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/Lj8-1dvazAU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Lj8-1dvazAU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <DogBreeds />
         </div>
     )

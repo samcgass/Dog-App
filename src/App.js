256c806b4a1d1d95444f48384b5320e2c038db4d
Sam Gass <samcgass@gmail.com>
2022-04-13 09:42:58 -0500

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar } from './components/navbar/Navbar';
+import { Home } from './components/home/Home';
+import { About } from './components/about/About';
+import { Contact } from './components/contact/Contact';
+import { NotFound } from './components/navbar/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
